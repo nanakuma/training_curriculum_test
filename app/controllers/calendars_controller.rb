012b3bad49cca103e059543df58c30e78c0a82fc
nanakuma <nanakumananakuma13096@gmail.com>
2020-11-24 12:25:20 +0900

    Issue1_3
    This reverts commit 7de2e735cd66f84b051af8a70afb7238d504646a.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -31,10 +31,11 @@ class CalendarsController < ApplicationController
 
     7.times do |x|
       today_plans = []
-      plan = plans.map do |plan|
+      plan = Plans.map do |plan|
+        #変更箇所P
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      days = { month : (@todays_date + x).month, date : (@todays_date+x).day, plans : today_plans}
       @week_days.push(days)
     end
 
